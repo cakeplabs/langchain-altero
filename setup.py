0e671cc55a8052ab0d9acd44e13b38b21395a2b5
dyudhani <dicky@alterolab.com>
2024-08-27 17:09:16 +0800

    update: version dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="langchain-altero",
-    version="0.0.2",
+    version="0.0.3",
     description="Streamline your AI workflows with langChain-altero, the essential toolkit for seamless LangChain integration.",
     long_description=long_description,
     long_description_content_type="text/markdown",
